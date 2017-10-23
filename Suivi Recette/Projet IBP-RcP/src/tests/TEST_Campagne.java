de03434279f9abf65970c96393a2e88fd2ad26ec
AnthonyCamusESAIP <anthony.camus@stjodijon.com>
2017-10-23 10:25:52 +0200

    Test implementation
    Implementation des fichiers de test et la suite de test => Reste à implémenter les méthodes de test

---
+++ Suivi Recette/Projet IBP-RcP/src/tests/TEST_Campagne.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class TEST_Campagne extends TestCase {
+
+	@Test
+	void testGetIdCampagne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIdCampagne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLabel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLabel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetProjet() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Suivi Recette/Projet IBP-RcP/src/tests/TEST_Test.java
@@ -0,0 +1,79 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class TEST_Test extends TestCase{
+
+	@Test
+	void testGetIdTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIdTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHeure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetHeure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetStatut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetStatut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetProjet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCampagne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCampagne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTesteur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTesteur() {
+		fail("Not yet implemented");
+	}
+
+}
