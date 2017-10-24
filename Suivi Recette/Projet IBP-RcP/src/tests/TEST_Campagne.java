ded96e7fd8839df0ba4e5bf7688784231d6c16f4
AnthonyCamusESAIP <anthony.camus@stjodijon.com>
2017-10-24 10:48:47 +0200

    Correction TU

--- Suivi Recette/Projet IBP-RcP/src/tests/TEST_Campagne.java
+++ Suivi Recette/Projet IBP-RcP/src/tests/TEST_Campagne.java
@@ -1,6 +1,6 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import beans.*;
 import junit.framework.TestCase;
--- Suivi Recette/Projet IBP-RcP/src/tests/TEST_Projet.java
+++ Suivi Recette/Projet IBP-RcP/src/tests/TEST_Projet.java
@@ -1,6 +1,6 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import junit.framework.TestCase;
 import beans.*;
