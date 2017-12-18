c2de087bf31707f0a00da175510666ba6b0856b8
AnthonyCamusESAIP <anthony.camus@stjodijon.com>
2017-12-18 15:27:07 +0100

    Module graphique : Pie OK
    Reste à ajuster les selects pour le graphe Line

--- SuiviRecette/Projet IBP-RcP/src/beans/ChartViewManager.java
+++ SuiviRecette/Projet IBP-RcP/src/beans/ChartViewManager.java
@@ -2,7 +2,12 @@ package beans;
 
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -10,6 +15,8 @@ import javax.faces.bean.ManagedBean;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.PieChartModel;
+
+import com.sun.glass.ui.Size;
  
 @ManagedBean
 public class ChartViewManager implements Serializable {
@@ -18,6 +25,27 @@ public class ChartViewManager implements Serializable {
     private LineChartModel lineModel;
 	private BarChartModel barModel;
 	private MysqlConnector mysqlConnect = new MysqlConnector("jdbc:mysql://localhost:3306/ibp-rcp", "root", "");
+	private List<String> testPassedThisWeek;
+	private List<String> testNAThisWeek;
+	private List<String> testFailedThisWeek;
+	private List<String> testNotCompletedThisWeek;
+	private int nbTestWeek1;
+	private int nbTestWeek2;
+	private int nbTestWeek3;
+	private int nbTestWeek4;
+	private int nbTestWeek5;
+	private String nomProjet;
+	private String date;
+	protected final static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public ChartViewManager(String _nomProjet, String _date) {
+		this.nomProjet = _nomProjet;
+		this.date = _date;
+	}
+	public ChartViewManager() {
+		this.date = "2017-12-22";
+		this.nomProjet ="GSP13044 - PPG - PARME - Sujets Prioritaires";
+	}
  
     @PostConstruct
     public void init() {
@@ -26,17 +54,50 @@ public class ChartViewManager implements Serializable {
     }
     
     public void initData() {
+    	testPassedThisWeek = new ArrayList<String>();
+    	testNAThisWeek = new ArrayList<String>();
+    	testFailedThisWeek = new ArrayList<String>();
+    	testNotCompletedThisWeek = new ArrayList<String>();
     	List<String> tables = new ArrayList<String>();
     	tables.add("projet");
     	tables.add("campagne");
     	tables.add("test");
     	List<String> attributs = new ArrayList<String>();
-    	attributs.add("nomProjet");
-    	attributs.add("nomCampagne");
-    	attributs.add("nomTest");
+    	attributs.add("test.statut");
+    	
+    	testDate();
 
-    	ArrayList<ArrayList<String>> data = mysqlConnect.MysqlSelect(tables, attributs, "");
-    	System.out.println(data);
+    	Calendar cal = Calendar.getInstance();
+    	try {
+			cal.setTime(df.parse(date));
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	for (ArrayList<String> string : mysqlConnect.MysqlSelect(tables, attributs, "nomProjet ='"+this.nomProjet+"' AND (Date BETWEEN '"+df.format(cal.getTime()).substring(0, 8)+(Integer.parseInt(df.format(cal.getTime()).substring(8))-4)+"' AND '"+date+"')")) {
+    		switch (string.get(0)) {
+			case "N/A":
+				testNAThisWeek.add(string.get(0));
+				break;
+			case "Passed":
+				testPassedThisWeek.add(string.get(0));
+				break;
+			case "Failed":
+				testFailedThisWeek.add(string.get(0));
+				break;
+			case "Not Completed":
+				testNotCompletedThisWeek.add(string.get(0));
+				break;
+			}
+		}
+    	//TODO: Ajuster les dates pour les counts
+    	nbTestWeek1 = mysqlConnect.MysqlSelect(tables, attributs, "nomProjet ='"+this.nomProjet+"' AND (Date BETWEEN '"+getMondayDate(date)+"' AND '"+date+"')").size();
+    	nbTestWeek2 = mysqlConnect.MysqlSelect(tables, attributs, "nomProjet ='"+this.nomProjet+"' AND (Date BETWEEN '"+getMondayDate(date)+"' AND '"+date+"')").size();
+    	nbTestWeek3 = mysqlConnect.MysqlSelect(tables, attributs, "nomProjet ='"+this.nomProjet+"' AND (Date BETWEEN '"+getMondayDate(date)+"' AND '"+date+"')").size();
+    	nbTestWeek4 = mysqlConnect.MysqlSelect(tables, attributs, "nomProjet ='"+this.nomProjet+"' AND (Date BETWEEN '"+getMondayDate(date)+"' AND '"+date+"')").size();
+    	nbTestWeek5 = mysqlConnect.MysqlSelect(tables, attributs, "nomProjet ='"+this.nomProjet+"' AND (Date BETWEEN '"+getMondayDate(date)+"' AND '"+date+"')").size();
     }
   
     public LineChartModel getLineModel() {
@@ -58,13 +119,19 @@ public class ChartViewManager implements Serializable {
  
     private void createPieModel() {
         pieModel = new PieChartModel();
-         
-        pieModel.set("Brand 1", 540);
-        pieModel.set("Brand 2", 325);
-        pieModel.set("Brand 3", 702);
-        pieModel.set("Brand 4", 421);
-         
-        pieModel.setTitle("Simple Pie");
+        if(testNAThisWeek.size() > 0) {
+            pieModel.set("NA", testNAThisWeek.size());
+        }
+        if(testPassedThisWeek.size() > 0) {
+            pieModel.set("Passed", testPassedThisWeek.size());
+        }
+        if(testFailedThisWeek.size() > 0) {
+            pieModel.set("Failed", testFailedThisWeek.size());
+        }
+        if(testNotCompletedThisWeek.size() > 0) {
+            pieModel.set("Not Completed", testNotCompletedThisWeek.size());
+        }         
+        pieModel.setTitle("Résultats des test");
         pieModel.setLegendPosition("w");
     }
     
@@ -74,6 +141,46 @@ public class ChartViewManager implements Serializable {
     
     private void createBarModel() {
     	barModel = new BarChartModel();
+    }
+    
+    private String getMondayDate(String _fridayDate) {
+    	Calendar cal = Calendar.getInstance();
+    	try {
+			cal.setTime(df.parse(_fridayDate));
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return (df.format(cal.getTime()).substring(0, 8)+(Integer.parseInt(df.format(cal.getTime()).substring(8))-4));
+    }
+    
+    private String getFridayLastWeek(String _fridayDate) {
+    	Calendar cal = Calendar.getInstance();
+    	try {
+			cal.setTime(df.parse(_fridayDate));
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return (df.format(cal.getTime()).substring(0, 8)+(Integer.parseInt(df.format(cal.getTime()).substring(8))-7));
+    }
+    
+    private void testDate() {
+    	
+    	Calendar cal = Calendar.getInstance();
+    	try {
+			cal.setTime(df.parse(date));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	System.out.println("Date initiale :" + df.format(cal.getTime()));
+    	cal.add(cal.DATE, -60);
+    	System.out.println("Date -60j :"+df.format(cal.getTime()));
+    	
     }
     
      
