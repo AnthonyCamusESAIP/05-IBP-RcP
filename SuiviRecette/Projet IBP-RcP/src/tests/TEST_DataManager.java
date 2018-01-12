d052b38f2e022da021cf3e5c5bd2417122f1f5b2
AlbanStJo <32925598+AlbanStJo@users.noreply.github.com>
2018-01-12 09:42:47 +0100

    Debut Test datamanager

---
+++ SuiviRecette/Projet IBP-RcP/src/tests/TEST_DataManager.java
@@ -0,0 +1,79 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TEST_DataManager {
+
+	@Test
+	public void testGetExistingProjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDataManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitExisting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitExistingTesteurs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitExistingVersions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitExistingProjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitExistingCampagnes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitExistingTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveImportedProjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveImportedCampagnes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveImportedTesteurs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveImportedTest() {
+		fail("Not yet implemented");
+	}
+
+}
