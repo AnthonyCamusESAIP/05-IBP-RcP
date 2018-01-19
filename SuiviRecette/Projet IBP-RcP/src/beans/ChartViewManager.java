6d346f55a2d7c88cf73cf2a1c524e5d7a592296d
AnthonyCamusESAIP <anthony.camus@stjodijon.com>
2018-01-19 15:30:21 +0100

    Update en cours

--- SuiviRecette/Projet IBP-RcP/src/beans/ChartViewManager.java
+++ SuiviRecette/Projet IBP-RcP/src/beans/ChartViewManager.java
@@ -427,8 +427,8 @@ public class ChartViewManager implements Serializable{
         if(testNotCompletedThisWeek.size() > 0) {
             pieModel.set("Not Completed", testNotCompletedThisWeek.size());
         }         
-        pieModel.setTitle("Résultats des tests");
         pieModel.setLegendPosition("w");
+        pieModel.setExtender("chartExtender");
     }
     private void createLineModel() {
     	
@@ -491,7 +491,6 @@ public class ChartViewManager implements Serializable{
         lineModel.addSeries(testPassed);
         lineModel.addSeries(testFailed);
         lineModel.addSeries(testNotCompleted);
-        lineModel.setTitle("Comparaison des test sur le mois passé");
         lineModel.setLegendPosition("n");
         lineModel.setShowPointLabels(true);
         lineModel.getAxes().put(AxisType.X, new CategoryAxis("Date"));
@@ -527,8 +526,7 @@ public class ChartViewManager implements Serializable{
  
         barModel.addSeries(thisWeek);
         barModel.addSeries(passedWeek);
-        
-        barModel.setTitle("Comparatif nombre de test total par rapport à la semaine dernière");
+       
         barModel.setLegendPosition("n");
          
         Axis xAxis = barModel.getAxis(AxisType.X);
@@ -537,7 +535,7 @@ public class ChartViewManager implements Serializable{
         Axis yAxis = barModel.getAxis(AxisType.Y);
         yAxis.setLabel("Tests");
         yAxis.setMin(0);
-        int[] tab = {nbTestThisWeek, nbTestWeek1};
+        int[] tab = {testFailedThisWeek.size()+testPassedThisWeek.size(), testFailedWeek1.size()+testPassedWeek1.size()};
         int max = Arrays.stream(tab).max().getAsInt();
         yAxis.setMax(max+5);
     }
@@ -573,7 +571,7 @@ public class ChartViewManager implements Serializable{
 		}
     	cal.add(Calendar.DAY_OF_MONTH, -30);
     	String beginPeriod = df.format(cal.getTime());
-    	if (mysqlConnect.verifyNumberDataPeriod(projectId, beginPeriod, date) > 0) {
+    	if (mysqlConnect.verifyNumberDataPeriod(projectId, beginPeriod, formatDate(event.getObject().toString())) > 0) {
         	date = formatDate(event.getObject().toString());
         	initData();
             createModels();
@@ -621,17 +619,9 @@ public class ChartViewManager implements Serializable{
     	double nbrTest = Integer.parseInt(nbrTests.get(0).get(0));
     	  
     	avancementMeteo = Math.round(((testPassed+(testFailed*(0.5)))/nbrTest)*100); 
-    	if(avancementMeteo >= 85){
-    		avancementImage = "sun.png";
-    	} else if (avancementMeteo >= 70){
-    		avancementImage = "sun-cloud.png";
-    	} else {
-    		avancementImage = "cloud-storm.png";
-    	}
     	
     }
-    
-	public void handleFileUpload(FileUploadEvent event) {
+    public void handleFileUpload(FileUploadEvent event) {
         if(event.getFile() != null) {
             file = new File(System.getProperty("java.io.tmpdir")+"lstTest.xls");
 			try {
@@ -650,6 +640,8 @@ public class ChartViewManager implements Serializable{
 			}
             if (dataManager != null) {
             	dataManager.saveData();
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(null, new FacesMessage("Successful",  "Your message: ") );
 			}
         }
         loadProjectList();
